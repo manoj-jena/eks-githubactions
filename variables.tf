9b0e7fa1fecf0c662963552ff1da30eed8e5fecf
manoj-jena <manoj.jena8118@gmail.com>
2023-09-28 11:08:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -66,7 +66,6 @@ variable "disk_size" {
 variable "instance_types" {
   description = "Set of instance types associated with the EKS Node Group. Defaults to `[\"t3.medium\"]`"
   type        = list(string)
-  default     = t2.medium
 }
 
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
