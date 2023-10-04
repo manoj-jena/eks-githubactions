de64c8c719f2dd818eefaa36b05c01b2adcb8072
manoj-jena <manoj.jena8118@gmail.com>
2023-10-04 07:26:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,10 @@ provider "kubernetes" {
 host = module.eks.cluster_endpoint
 cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
-  
+locals {
+  cluster_name = var.cluster_name
+  owner        = "TSI"
+}
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Module for creating a VPC
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -14,7 +17,7 @@ version = "5.0.0"
 name = var.vpc_name
 
 cidr = "10.0.0.0/16"
-azs = slice(data.aws_availability_zones.available.names, 0, 3)
+azs = slice(data.aws_availability_zones.available.names, 0, var.availability_zones_count)
 
 private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -43,7 +46,7 @@ source = "terraform-aws-modules/eks/aws"
 version = "~> 19.0"
 
 cluster_name = var.cluster_name
-cluster_version = "1.27"
+cluster_version = var.eks_cluster_version
 
 vpc_id = module.vpc.vpc_id
 subnet_ids = module.vpc.private_subnets
