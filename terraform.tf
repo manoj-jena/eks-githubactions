510205ecd021e5dfe2e1dad4c52a69f3cd9a50d2
manoj-jena <manoj.jena8118@gmail.com>
2023-10-04 06:52:49 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,29 @@
+terraform {
+required_version = ">= 1.0.0"
+
+required_providers {
+aws = {
+source = "hashicorp/aws"
+}
+
+random = {
+source = "hashicorp/random"
+version = "~> 3.4.3"
+}
+
+tls = {
+source = "hashicorp/tls"
+version = "~> 4.0.4"
+}
+
+cloudinit = {
+source = "hashicorp/cloudinit"
+version = "~> 2.2.0"
+}
+
+kubernetes = {
+source = "hashicorp/kubernetes"
+version = "~> 2.16.1"
+}
+}
+}
