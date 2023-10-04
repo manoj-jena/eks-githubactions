9ee7fa4c3d475510e10d72cf3c6d815eda1f32f2
manoj-jena <manoj.jena8118@gmail.com>
2023-10-04 22:50:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -88,5 +88,14 @@ variable "subnet_cidr_bits" {
   type        = number
   default     = 8
 }
-
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-your_first_subnet_id"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-your_second_subnet_id"
+ }
 
